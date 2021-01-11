478af1a6dcae8d0a2b45a27c1b44886a33921732
Heshan Thenura Kariyawasam <75155192+heshanthenura@users.noreply.github.com>
2021-01-11 21:53:43 +0530

    Add files via upload

---
+++ js/clipboard.js
@@ -0,0 +1,5 @@
+document.getElementsByName("git-clone").addEventListener("click", myFunction);
+
+function myFunction() {
+    alert('Git Clone Link Copied to Clipboard\n\nhttps://github.com/heshanthenura/Maths.git');
+  }
