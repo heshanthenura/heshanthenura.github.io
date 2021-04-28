f120f17a0124f237bec6b1f53f5c4c9893e3782c
Heshan Thenura Kariyawasam <75155192+heshan-thenura@users.noreply.github.com>
2021-04-28 20:14:42 +0530

    Update

--- js/clipboard.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByName("git-clone").addEventListener("click", myFunction);
-
-function myFunction() {
-    alert('Git Clone Link Copied to Clipboard\n\nhttps://github.com/heshanthenura/Maths.git');
-  }
